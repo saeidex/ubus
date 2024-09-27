f759e18c6aa258b9092945f6a20c0305c2e4ca9d
saeidex <ebrahim.khalil.nayeem@gmail.com>
2024-09-28 00:28:41 +0600

    refactor(nextjs): breaking into inner components

--- apps/nextjs/src/app/_components/our-team.tsx
+++ apps/nextjs/src/app/_components/our-team.tsx
@@ -6,53 +6,8 @@ import {
   IconBrandXFilled,
 } from "@tabler/icons-react";
 
-interface ITeamMember {
-  name: string;
-  usernames: { github: string; linkedin: string; x: string };
-}
-
-const teamMembers: ITeamMember[] = [
-  {
-    name: "Hasibur Rahman",
-    usernames: {
-      github: "inbox-hasibur",
-      linkedin: "inboxhasibur",
-      x: "inboxhasibur",
-    },
-  },
-  {
-    name: "Sazidul Islam Sazid",
-    usernames: {
-      github: "sazidul",
-      linkedin: "",
-      x: "",
-    },
-  },
-  {
-    name: "Muhammad Rony",
-    usernames: {
-      github: "rony",
-      linkedin: "",
-      x: "",
-    },
-  },
-  {
-    name: "Hijbullah",
-    usernames: {
-      github: "hijbullah",
-      linkedin: "",
-      x: "",
-    },
-  },
-  {
-    name: "EK Nayeem",
-    usernames: {
-      github: "saeidex",
-      linkedin: "saeidex",
-      x: "saeidex_",
-    },
-  },
-];
+import type { ITeamMember } from "@ubus/configs";
+import { teamMembers } from "@ubus/configs";
 
 export const OurTeam = () => {
   return (
@@ -70,57 +25,67 @@ export const OurTeam = () => {
           </p>
         </div>
         <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-          {teamMembers.map((item) => (
-            <div
-              key={item.usernames.github}
-              className="flex flex-col gap-3 overflow-hidden rounded-lg bg-surface-variant pb-5 text-on-surface-variant hover:shadow-lg"
-            >
-              <div className="aspect-square max-h-full overflow-hidden rounded-lg bg-surface-container-high object-cover">
-                <Image
-                  src={`https://github.com/${item.usernames.github}.png`}
-                  width={400}
-                  height={400}
-                  alt="profile image"
-                />
-              </div>
-              <h4 className="text-center text-body-small font-bold sm:text-body-large">
-                {item.name}
-              </h4>
-              <div className="flex items-center justify-center gap-3">
-                <Link
-                  target="_blank"
-                  href={{
-                    protocol: "https",
-                    hostname: "linkedin.com",
-                    pathname: `/in/${item.usernames.linkedin}`,
-                  }}
-                >
-                  <IconBrandLinkedin size={20} />
-                </Link>
-                <Link
-                  target="_blank"
-                  href={{
-                    protocol: "https",
-                    hostname: "x.com",
-                    pathname: `/${item.usernames.x}`,
-                  }}
-                >
-                  <IconBrandXFilled size={20} />
-                </Link>
-                <Link
-                  target="_blank"
-                  href={{
-                    protocol: "https",
-                    hostname: "github.com",
-                    pathname: `/${item.usernames.github}`,
-                  }}
-                >
-                  <IconBrandGithub size={20} />
-                </Link>
-              </div>
-            </div>
+          {teamMembers.map((member) => (
+            <TeamMemberCard item={member} key={member.usernames.github} />
           ))}
         </div>
+      </div>
+    </div>
+  );
+};
+
+const TeamMemberCard = ({ item }: { item: ITeamMember }) => {
+  return (
+    <div
+      key={item.usernames.github}
+      className="flex flex-col gap-3 overflow-hidden rounded-lg bg-surface-variant pb-5 text-on-surface-variant hover:shadow-lg"
+    >
+      <div className="aspect-square max-h-full overflow-hidden rounded-lg bg-surface-container-high object-cover">
+        <Image
+          src={`https://github.com/${item.usernames.github}.png`}
+          width={400}
+          height={400}
+          alt="profile image"
+        />
+      </div>
+      <h4 className="text-center text-body-small font-bold sm:text-body-large">
+        {item.name}
+      </h4>
+      <div className="flex items-center justify-center gap-3">
+        {item.usernames.linkedin && (
+          <Link
+            target="_blank"
+            href={{
+              protocol: "https",
+              hostname: "linkedin.com",
+              pathname: `/in/${item.usernames.linkedin}`,
+            }}
+          >
+            <IconBrandLinkedin size={20} />
+          </Link>
+        )}
+        {item.usernames.x && (
+          <Link
+            target="_blank"
+            href={{
+              protocol: "https",
+              hostname: "x.com",
+              pathname: `/${item.usernames.x}`,
+            }}
+          >
+            <IconBrandXFilled size={20} />
+          </Link>
+        )}
+        <Link
+          target="_blank"
+          href={{
+            protocol: "https",
+            hostname: "github.com",
+            pathname: `/${item.usernames.github}`,
+          }}
+        >
+          <IconBrandGithub size={20} />
+        </Link>
       </div>
     </div>
   );
