23955abd51f2db6cf37584e8cb0e9616b3188734
saeidex <ebrahim.khalil.nayeem@gmail.com>
2024-10-24 00:19:40 +0600

    fix: local development font loading problem

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -19,6 +19,7 @@ export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   variable: "--font-poppins",
+  preload: true,
 });
 
 export const metadata: Metadata = {
