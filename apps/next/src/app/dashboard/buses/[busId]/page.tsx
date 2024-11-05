be455184e6ddc89d0736c9772577e721c5418c79
saeidex <ebrahim.khalil.nayeem@gmail.com>
2024-11-05 11:30:02 +0600

    refactor: typo

--- apps/next/src/app/dashboard/buses/[busId]/page.tsx
+++ apps/next/src/app/dashboard/buses/[busId]/page.tsx
@@ -4,7 +4,7 @@ import { MqttProvider, useMqttSubscription } from "@ubus/mqtt";
 
 interface BusLocationPageProps {
   params: {
-    bussId: string;
+    busId: string;
   };
 }
 
@@ -17,7 +17,7 @@ const BusLocationPage = (props: BusLocationPageProps) => {
 };
 
 const BusLocationContent = (props: BusLocationPageProps) => {
-  const { data: location, error } = useMqttSubscription(props.params.bussId);
+  const { data: location, error } = useMqttSubscription(props.params.busId);
 
   // if (isLoading) return <p>Loading...</p>;
   if (error)
